622c8befb2548a08c7120d3933d20750f7cc8c73
Mahbub <mahbubhussaincse@gmail.com>
2023-08-12 19:47:02 +0600

    code optimised

--- includes/Admin/Settings/views/form-fields.php
+++ includes/Admin/Settings/views/form-fields.php
@@ -4,63 +4,31 @@
         <?php wp_nonce_field('form_fields_settings_nonce', 'form_fields_settings_nonce'); ?>
         <table class="form-table epm-th">
             <tbody>
-                <tr>
-                    <th><label for="epm_form_heading_name"><?php _e("Modify Form Heading 'Name'", 'eco-profile-master'); ?></label></th>
-                    <td><input type="text" class="regular-text" id="epm_form_heading_name" name="epm_form_heading_name" value="<?php echo esc_attr(sanitize_text_field(get_option('epm_form_heading_name', 'Name')) ?? ''); ?>"></td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_name_hide"><?php _e("Checked Form Heading 'Name' ", 'eco-profile-master'); ?></label></th>
-                    <td>
-                        <input type="checkbox" id="epm_form_heading_name_hide" name="epm_form_heading_name_hide" value="1" <?php checked(get_option('epm_form_heading_name_hide', '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
-                        <p class="epm-description"><?php _e('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master'); ?></p>
-                    </td>
-                </tr>
+                <?php
+                $sections = array(
+                    'name' => __('Name', 'eco-profile-master'),
+                    'contact_info' => __('Contact Info', 'eco-profile-master'),
+                    'about_yourself' => __('About Yourself', 'eco-profile-master'),
+                    'profile_image' => __('Profile Image', 'eco-profile-master'),
+                    'social_links' => __('Social Links', 'eco-profile-master'),
+                );
 
-                <tr>
-                    <th><label for="epm_form_heading_contact_info"><?php _e("Modify Form Heading 'Contact Info'", 'eco-profile-master'); ?></label></th>
-                    <td><input type="text" class="regular-text" id="epm_form_heading_contact_info" name="epm_form_heading_contact_info" value="<?php echo esc_attr(sanitize_text_field(get_option('epm_form_heading_contact_info', 'Contact Info')) ?? ''); ?>"></td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_contact_info_hide"><?php _e("Checked Form Heading 'Contact Info' ", 'eco-profile-master'); ?></label></th>
-                    <td>
-                        <input type="checkbox" id="epm_form_heading_contact_info_hide" name="epm_form_heading_contact_info_hide" value="1" <?php checked(get_option('epm_form_heading_contact_info_hide', '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
-                        <p class="epm-description"><?php _e('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master'); ?></p>
-                    </td>
-                </tr>
-
-                <tr>
-                    <th><label for="epm_form_heading_about_yourself"><?php _e("Modify Form Heading 'About Yourself'", 'eco-profile-master'); ?></label></th>
-                    <td><input type="text" class="regular-text" id="epm_form_heading_about_yourself" name="epm_form_heading_about_yourself" value="<?php echo esc_attr(sanitize_text_field(get_option('epm_form_heading_about_yourself', 'About Yourself')) ?? ''); ?>"></td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_about_yourself_hide"><?php _e("Checked Form Heading 'About Yourself' ", 'eco-profile-master'); ?></label></th>
-                    <td>
-                        <input type="checkbox" id="epm_form_heading_about_yourself_hide" name="epm_form_heading_about_yourself_hide" value="1" <?php checked(get_option('epm_form_heading_about_yourself_hide', '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
-                        <p class="epm-description"><?php _e('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master'); ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_profile_image"><?php _e("Modify Form Heading 'Profile Image'", 'eco-profile-master'); ?></label></th>
-                    <td><input type="text" class="regular-text" id="epm_form_heading_profile_image" name="epm_form_heading_profile_image" value="<?php echo esc_attr(sanitize_text_field(get_option('epm_form_heading_profile_image', 'Profile Image')) ?? ''); ?>"></td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_profile_image_hide"><?php _e("Checked Form Heading 'Profile Image' ", 'eco-profile-master'); ?></label></th>
-                    <td>
-                        <input type="checkbox" id="epm_form_heading_profile_image_hide" name="epm_form_heading_profile_image_hide" value="1" <?php checked(get_option('epm_form_heading_profile_image_hide', '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
-                        <p class="epm-description"><?php _e('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master'); ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_social_links"><?php _e("Modify Form Heading 'Social Links'", 'eco-profile-master'); ?></label></th>
-                    <td><input type="text" class="regular-text" id="epm_form_heading_social_links" name="epm_form_heading_social_links" value="<?php echo esc_attr(sanitize_text_field(get_option('epm_form_heading_social_links', 'Social Links')) ?? ''); ?>"></td>
-                </tr>
-                <tr>
-                    <th><label for="epm_form_heading_social_links"><?php _e("Checked Form Heading 'Social Links' ", 'eco-profile-master'); ?></label></th>
-                    <td>
-                        <input type="checkbox" id="epm_form_heading_social_links_hide" name="epm_form_heading_social_links_hide" value="1" <?php checked(get_option('epm_form_heading_social_links_hide', '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
-                        <p class="epm-description"><?php _e('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master'); ?></p>
-                    </td>
-                </tr>
+                foreach ($sections as $section_key => $section_label) :
+                    $option_name = "epm_form_heading_$section_key";
+                    $hide_option_name = "epm_form_heading_{$section_key}_hide";
+                ?>
+                    <tr>
+                        <th><label for="<?php echo esc_attr($option_name); ?>"><?php printf(__('Modify Form Heading \'%s\'', 'eco-profile-master'), esc_html($section_label)); ?></label></th>
+                        <td><input type="text" class="regular-text" id="<?php echo esc_attr($option_name); ?>" name="<?php echo esc_attr($option_name); ?>" value="<?php echo esc_attr(sanitize_text_field(get_option($option_name, $section_label) ?? '')); ?>"></td>
+                    </tr>
+                    <tr>
+                        <th><label for="<?php echo esc_attr($hide_option_name); ?>"><?php printf(__('Checked Form Heading \'%s\'', 'eco-profile-master'), esc_html($section_label)); ?></label></th>
+                        <td>
+                            <input type="checkbox" id="<?php echo esc_attr($hide_option_name); ?>" name="<?php echo esc_attr($hide_option_name); ?>" value="1" <?php checked(get_option($hide_option_name, '1'), 1); ?>><?php esc_html_e('Show or Hide', 'eco-profile-master'); ?>
+                            <p class="epm-description"><?php printf(__('By checking the heading won\'t be shown, Default is Show.', 'eco-profile-master')); ?></p>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
         <p class="submit">
